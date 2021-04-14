65be5e329ac89c1ea51353350c7aa2ce4e015028
luanaterense <luana.r.matias@gmail.com>
2021-04-14 15:21:51 -0300

    Inclusão de relatórios, vídeo e screenshots

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,7 @@
  const cucumber = require('cypress-cucumber-preprocessor').default
+ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
  module.exports = (on, config) => {
    on('file:preprocessor', cucumber())
+   allureWriter(on, config);
+    return config;
  }
- 
--- cypress/support/page/PesquisaQAPage.js
+++ cypress/support/page/PesquisaQAPage.js
@@ -5,7 +5,8 @@ const url = Cypress.config("baseUrl")
 class PesquisaQAPage{
 
     acessarURL(){
-        cy.visit(url);
+        cy.visit(url)
+        cy.screenshot('Página acessada')
     }
 
     clicarNoMenuPesquisaQA(menu){
@@ -20,12 +21,13 @@ class PesquisaQAPage{
         cy.get(pesquisaQAElements.email()).type('luana.r.matias@gmail.com')
         cy.get(pesquisaQAElements.confirmEmail()).type('luana.r.matias@gmail.com')
         cy.get(pesquisaQAElements.phone()).type('(19) 98140-2803')
-        cy.get(pesquisaQAElements.idade()).click();
+        cy.get(pesquisaQAElements.idade()).click()
         cy.get(pesquisaQAElements.anosExperienciaQA()).select('mais de 5 anos')
         cy.get(pesquisaQAElements.atracaoPelaArea()).select('Desafio')
         cy.get(pesquisaQAElements.precisaMelhorar()).click()
         cy.get(pesquisaQAElements.linguagens()).type('Javascript, Java, C#')
         cy.get(pesquisaQAElements.planoCarreira()).type('Me tornar uma profissional espelho, onde eu possa estimular as pessoas a trabalharem nessa área tão ampla e maravilhosa.')
+        cy.screenshot('Campos preenchidos')
     }
 
     clicarEmEnviar(){
@@ -34,6 +36,7 @@ class PesquisaQAPage{
 
     validarEnvio(){
         cy.get(pesquisaQAElements.mensagemFinal()).should('contain', 'Your form has been successfully submitted.')
+        cy.screenshot('Validação do envio')
     }
 }
 
