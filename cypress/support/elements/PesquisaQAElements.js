274f75b0114cfb61d98c1cd473d1953b51d64c63
luanaterense <luana.r.matias@gmail.com>
2021-04-14 23:30:10 -0300

    Preencher campos obrigatórios somente

--- cypress/support/elements/PesquisaQAElements.js
+++ cypress/support/elements/PesquisaQAElements.js
@@ -8,13 +8,13 @@ class PesquisaQAElements{
     sobrenome = () => {return 'input[name="lname"]'}
     email = () => {return 'input[name="email"]'}
     confirmEmail = () => {return '#nf-field-8'}
-    phone = () => {return 'input[name="phone"]'}
+    phone = () => {return 'input[name="phone"]'} //Não obrigatório
     idade = () => {return '#nf-label-class-field-10-0'}
     anosExperienciaQA = () => {return '#nf-field-11'}
     atracaoPelaArea = () => {return '#nf-field-12'}
     precisaMelhorar = () => {return '#nf-label-class-field-13-0'}
     linguagens = () => {return '#nf-field-14'}
-    planoCarreira = () => {return '#nf-field-15'}
+    planoCarreira = () => {return '#nf-field-15'} //Não obrigatório
 
     //Botão Enviar
     enviar = () => {return "#nf-field-16"}
--- cypress/support/page/PesquisaQAPage.js
+++ cypress/support/page/PesquisaQAPage.js
@@ -15,18 +15,17 @@ class PesquisaQAPage{
         .click()
     }
 
+
     preencherCampos(){
         cy.get(pesquisaQAElements.nome()).type('Luana')
         cy.get(pesquisaQAElements.sobrenome()).type('Terense')
         cy.get(pesquisaQAElements.email()).type('luana.r.matias@gmail.com')
         cy.get(pesquisaQAElements.confirmEmail()).type('luana.r.matias@gmail.com')
-        cy.get(pesquisaQAElements.phone()).type('(19) 98140-2803')
         cy.get(pesquisaQAElements.idade()).click()
         cy.get(pesquisaQAElements.anosExperienciaQA()).select('mais de 5 anos')
         cy.get(pesquisaQAElements.atracaoPelaArea()).select('Desafio')
         cy.get(pesquisaQAElements.precisaMelhorar()).click()
         cy.get(pesquisaQAElements.linguagens()).type('Javascript, Java, C#')
-        cy.get(pesquisaQAElements.planoCarreira()).type('Me tornar uma profissional espelho, onde eu possa estimular as pessoas a trabalharem nessa área tão ampla e maravilhosa.')
         cy.screenshot('Campos preenchidos')
     }
 
